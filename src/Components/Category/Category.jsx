943df063aa0bb4f670e6c199e251715bb8238231
SaddamHossain07 <contact.saddam3@gmail.com>
2023-11-06 11:43:49 +0600

    adding get in touch in home page

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -17,32 +17,34 @@ const Category = () => {
                     </Marquee>
                 </div>
             </div>
-            <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-16">
-                <div className="shadow-lg border-2 rounded-lg border-slate-200 h-[350px] relative overflow-hidden">
-                    <img className="w-full h-[300px] z-30" src="https://i.ibb.co/Lp8pWTy/download.jpg" alt="" />
-                    <div className="h-[150px] -skew-y-6 hover:skew-y-0 w-full bg-white -bottom-10 hover:-bottom-5 z-20 absolute"></div>
+            <Marquee speed={20}>
+                <div className="grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mt-16">
+                    <div className="shadow-lg w-[300px] border-2 rounded-lg border-slate-200 h-[350px] relative overflow-hidden">
+                        <img className="w-full h-[300px] z-30" src="https://i.ibb.co/Lp8pWTy/download.jpg" alt="" />
+                        <div className="h-[150px] -skew-y-6 hover:skew-y-0 w-full bg-white -bottom-10 hover:-bottom-5 z-20 absolute"></div>
 
-                    <h3 className="text-3xl font-bold text-center z-50 inline-block bottom-12 left-6 absolute">Novel</h3>
-                </div>
-                <div className="shadow-lg border-2 rounded-lg border-slate-200 h-[350px] relative overflow-hidden">
-                    <img className="w-full h-[300px] z-30" src="https://i.ibb.co/VHDSs2x/thrilled.png" alt="" />
-                    <div className="h-[150px] -skew-y-6 hover:skew-y-0 w-full bg-white -bottom-10 hover:-bottom-5 z-20 absolute"></div>
+                        <h3 className="text-3xl font-bold text-center z-50 inline-block bottom-12 left-6 absolute">Novel</h3>
+                    </div>
+                    <div className="shadow-lg w-[300px] border-2 rounded-lg border-slate-200 h-[350px] relative overflow-hidden">
+                        <img className="w-full h-[300px] z-30" src="https://i.ibb.co/VHDSs2x/thrilled.png" alt="" />
+                        <div className="h-[150px] -skew-y-6 hover:skew-y-0 w-full bg-white -bottom-10 hover:-bottom-5 z-20 absolute"></div>
 
-                    <h3 className="text-3xl font-bold text-center z-50 inline-block bottom-12 left-6 absolute">Thriller</h3>
-                </div>
-                <div className="shadow-lg border-2 rounded-lg border-slate-200 h-[350px] relative overflow-hidden">
-                    <img className="w-full h-[300px] z-30" src="https://i.ibb.co/CBVdnXN/history-2.jpg" alt="" />
-                    <div className="h-[150px] -skew-y-6 hover:skew-y-0 w-full bg-white -bottom-10 hover:-bottom-5 z-20 absolute"></div>
+                        <h3 className="text-3xl font-bold text-center z-50 inline-block bottom-12 left-6 absolute">Thriller</h3>
+                    </div>
+                    <div className="shadow-lg w-[300px] border-2 rounded-lg border-slate-200 h-[350px] relative overflow-hidden">
+                        <img className="w-full h-[300px] z-30" src="https://i.ibb.co/CBVdnXN/history-2.jpg" alt="" />
+                        <div className="h-[150px] -skew-y-6 hover:skew-y-0 w-full bg-white -bottom-10 hover:-bottom-5 z-20 absolute"></div>
 
-                    <h3 className="text-3xl font-bold text-center z-50 inline-block bottom-12 left-6 absolute">History</h3>
-                </div>
-                <div className="shadow-lg border-2 rounded-lg border-slate-200 h-[350px] relative overflow-hidden">
-                    <img className="w-full h-[300px] z-30" src="https://i.ibb.co/KrkpJ5p/images-3.jpg" alt="" />
-                    <div className="h-[150px] -skew-y-6 hover:skew-y-0 w-full bg-white -bottom-10 hover:-bottom-5 z-20 absolute"></div>
+                        <h3 className="text-3xl font-bold text-center z-50 inline-block bottom-12 left-6 absolute">History</h3>
+                    </div>
+                    <div className="shadow-lg w-[300px] border-2 rounded-lg border-slate-200 h-[350px] relative overflow-hidden">
+                        <img className="w-full h-[300px] z-30" src="https://i.ibb.co/KrkpJ5p/images-3.jpg" alt="" />
+                        <div className="h-[150px] -skew-y-6 hover:skew-y-0 w-full bg-white -bottom-10 hover:-bottom-5 z-20 absolute"></div>
 
-                    <h3 className="text-3xl font-bold text-center z-50 inline-block bottom-12 left-6 absolute">Drama</h3>
+                        <h3 className="text-3xl font-bold text-center z-50 inline-block bottom-12 left-6 absolute">Drama</h3>
+                    </div>
                 </div>
-            </div>
+            </Marquee>
         </div>
     );
 };
--- src/Components/GetInTouch/GetInTouch.jsx
+++ src/Components/GetInTouch/GetInTouch.jsx
@@ -1,14 +1,25 @@
-import bgImg from '../../assets/images/World Map.svg'
+
+import { FaGreaterThan } from 'react-icons/fa';
 const GetInTouch = () => {
     return (
-        <div className="my-24 h-52 w-full bg-orange-200 relative overflow-hidden">
-            <img className="w-full h-full object-cover opacity-50" src="https://i.ibb.co/vmQLz4h/liquid-cheese-1.png" alt="" />
+        <div className="my-24 h-56 w-ful relative overflow-hidden">
+            <img className="w-full h-full object-cover " src="https://i.ibb.co/9bzMd11/get-In-Touch.png" alt="" />
+            {/* <img className="w-full h-full object-cover opacity-50" src="https://i.ibb.co/vmQLz4h/liquid-cheese-1.png" alt="" /> */}
 
             {/* <img className='w-full h-full object-cover' src={bgImg} alt="" /> */}
 
-            <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#d28328e1] to-[rgba(21, 21, 21, 0)]">
-
-                <h3 className="text-5xl text-white font-bold ml-10">Top Books By Best Authors</h3>
+            <div className="absolute flex items-center border-2 w-full h-full left-0 top-0 bg-gradient-to-r from-[#d28328e1] to-[rgba(21, 21, 21, 0)] px-10">
+                <div className='w-3/4'>
+                    <h3 className="text-5xl text-white font-bold">Top Books By Best Authors</h3>
+                </div>
+                <div className='h-20 w-1/4 flex text-white text-xl font-bold'>
+                    <div className='bg-orange-600 items-center flex justify-center w-3/4'>
+                        <h3>GET IN TOUCH</h3>
+                    </div>
+                    <button className='bg-orange-500 hover:bg-orange-700 items-center flex justify-center w-1/4'>
+                        <FaGreaterThan />
+                    </button>
+                </div>
 
             </div>
 
